fea21159cabe0c17621a9a14f516c1863728523c
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2020-04-26 11:51:16 -0700

    Make (integration) test(s) work on nix
    This commit also adds FluentAssertions.
    
    This commit solves #2.

--- Source/Tests/LehmanLaidun.Console.Test/ConsoleTest.cs
+++ Source/Tests/LehmanLaidun.Console.Test/ConsoleTest.cs
@@ -1,4 +1,6 @@
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,41 +11,80 @@ namespace LehmanLaidun.Console.Test
     public class ConsoleTest
     {
         [TestMethod]
-        public void Renameme() // TODO:OF:Rename.
+        public void CompareTwoFolders()
         {
-            const string expectedOutput =
+            var expectedOutput =
 @"Found only in first:/root/directory[@name='Summer 2018']/file[@name='cat.jpg' and @length='9452'].
 Found only in first:/root/directory[@name='Summer 2018']/file[@name='museum.jpg' and @length='6618'].
 Found only in second:/root/directory[@name='Summer 2018']/file[@name='Birds at pier.jfif' and @length='5327'].
-";
+".NormaliseLineEndings();
+
+
+            var arguments = Path.Combine(FindDataPath(), "MyDrive") + " " + Path.Combine(FindDataPath(), "TheirDrive");
             string output;
+
+            //  Act.
             using (var p = new Process())
             {
-                p.StartInfo.FileName = FindConsoleExe();
-                p.StartInfo.WorkingDirectory = FindTesteeSlnPath();
-                p.StartInfo.Arguments = @".\Data\MyDrive\ .\Data\TheirDrive\";
+                p.StartInfo.FileName = FindConsoleExecutable();
+                p.StartInfo.WorkingDirectory = FindTesteePrjPath();
+                p.StartInfo.Arguments = arguments;
                 p.StartInfo.UseShellExecute = false;
                 p.StartInfo.RedirectStandardOutput = true;
                 p.Start();
                 output = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
+                if (p.ExitCode != 0)
+                {
+                    output = output + Environment.NewLine + $"ExitCode=[{p.ExitCode}]{Environment.NewLine}{output}";
+                }
             }
-            Assert.AreEqual(expectedOutput, output);
+
+            //  Assert.
+            output.NormaliseLineEndings()
+                .Should().Be(expectedOutput);
         }
 
-        private static string FindConsoleExe()
+        private static string FindConsoleBinPath()
         {
-            var currentDirectory = Directory.GetCurrentDirectory(); // Get the test directory.
-            var currentRoot = Path.Combine(currentDirectory, @"..\..\..\..\..");    // Go to the closes common parent.
-            var exeFileName = Directory.GetFiles(Path.Combine(currentRoot, @"LehmanLaidun.Console\bin\debug\netcoreapp3.1")
-                ).First(x => x.EndsWith(".exe"));   // Drill down and hope for the dotnet version and file name to as anticipated.
+#if DEBUG
+            var releaseDebugFolder = "Debug";
+#endif
+#if RELEASE
+            var releaseDebugFolder = "Release";
+#endif
+            var exeDirectory = Path.Combine(FindTesteePrjPath(), "bin", releaseDebugFolder, "netcoreapp3.1");
+            return exeDirectory;
+        }
+
+        private static string FindConsoleExecutable()
+        {
+            var fileEnding = Environment.OSVersion.Platform switch
+            {
+                PlatformID.Win32NT => ".exe",
+                PlatformID.Win32Windows => ".exe",  //  Not tested.
+                PlatformID.Unix => ".Console",
+                PlatformID.MacOSX => ".Console",    // Not tested.
+                PlatformID.Win32S => throw new NotImplementedException(),
+                PlatformID.WinCE => throw new NotImplementedException(),
+                PlatformID.Xbox => throw new NotImplementedException(),
+                _ => throw new NotImplementedException(),
+            };
+            var exeFileName = Directory.GetFiles(FindConsoleBinPath())
+                .First(x => x.EndsWith(fileEnding));   // Drill down and hope for the dotnet version and file name to as anticipated.
             return exeFileName;
         }
 
-        private static string FindTesteeSlnPath()
+        private static string FindDataPath()
+        {
+            var exeDirectory = Path.Combine(FindTesteePrjPath(), "Data");
+            return exeDirectory;
+        }
+
+        private static string FindTesteePrjPath()
         {
             var currentDirectory = Directory.GetCurrentDirectory(); // Get the test directory.
-            var currentRoot = Path.Combine(currentDirectory, @"..\..\..\..\..");    // Go to the closes common parent.
+            var currentRoot = Path.Combine(currentDirectory, "..", "..", "..", "..", "..");    // Go to the closes common parent.
             var slnPath = Path.Combine(currentRoot, "LehmanLaidun.Console");
             return slnPath;
         }
---
+++ Source/Tests/LehmanLaidun.Console.Test/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace LehmanLaidun.Console.Test
+{
+    internal static class StringExtensions
+    {
+        internal static string NormaliseLineEndings( this string s)
+        {
+            return s.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", "\r\n");
+        }
+    }
+}
