1327c21458507b75cd43b348869952fb73de103f
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2020-08-01 19:54:23 +0000

    Move helper methods out of test

--- Source/Tests/LehmanLaidun.Console.Test/ConsoleTest.cs
+++ Source/Tests/LehmanLaidun.Console.Test/ConsoleTest.cs
@@ -1,9 +1,6 @@
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 
 namespace LehmanLaidun.Console.Test
 {
@@ -19,74 +16,14 @@ Found only in first:/root/directory[@name='Summer 2018']/file[@name='museum.jpg'
 Found only in second:/root/directory[@name='Summer 2018']/file[@name='Birds at pier.jfif' and @length='5327'].
 ".NormaliseLineEndings();
 
-
-            var arguments = Path.Combine(FindDataPath(), "MyDrive") + " " + Path.Combine(FindDataPath(), "TheirDrive");
-            string output;
+            var arguments = Path.Combine(TheFileSystem.FindDataPath(), "MyDrive") + " " + Path.Combine(TheFileSystem.FindDataPath(), "TheirDrive");
 
             //  Act.
-            using (var p = new Process())
-            {
-                p.StartInfo.FileName = FindConsoleExecutable();
-                p.StartInfo.WorkingDirectory = FindTesteePrjPath();
-                p.StartInfo.Arguments = arguments;
-                p.StartInfo.UseShellExecute = false;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.Start();
-                output = p.StandardOutput.ReadToEnd();
-                p.WaitForExit();
-                if (p.ExitCode != 0)
-                {
-                    output = output + Environment.NewLine + $"ExitCode=[{p.ExitCode}]{Environment.NewLine}{output}";
-                }
-            }
+            var output = TheFileSystem.Execute(arguments);
 
             //  Assert.
             output.NormaliseLineEndings()
                 .Should().Be(expectedOutput);
-        }
-
-        private static string FindConsoleBinPath()
-        {
-#if DEBUG
-            var releaseDebugFolder = "Debug";
-#endif
-#if RELEASE
-            var releaseDebugFolder = "Release";
-#endif
-            var exeDirectory = Path.Combine(FindTesteePrjPath(), "bin", releaseDebugFolder, "netcoreapp3.1");
-            return exeDirectory;
-        }
-
-        private static string FindConsoleExecutable()
-        {
-            var fileEnding = Environment.OSVersion.Platform switch
-            {
-                PlatformID.Win32NT => ".exe",
-                PlatformID.Win32Windows => ".exe",  //  Not tested.
-                PlatformID.Unix => ".Console",
-                PlatformID.MacOSX => ".Console",    // Not tested.
-                PlatformID.Win32S => throw new NotImplementedException(),
-                PlatformID.WinCE => throw new NotImplementedException(),
-                PlatformID.Xbox => throw new NotImplementedException(),
-                _ => throw new NotImplementedException(),
-            };
-            var exeFileName = Directory.GetFiles(FindConsoleBinPath())
-                .First(x => x.EndsWith(fileEnding));   // Drill down and hope for the dotnet version and file name to as anticipated.
-            return exeFileName;
-        }
-
-        private static string FindDataPath()
-        {
-            var exeDirectory = Path.Combine(FindTesteePrjPath(), "Data");
-            return exeDirectory;
-        }
-
-        private static string FindTesteePrjPath()
-        {
-            var currentDirectory = Directory.GetCurrentDirectory(); // Get the test directory.
-            var currentRoot = Path.Combine(currentDirectory, "..", "..", "..", "..", "..");    // Go to the closes common parent.
-            var slnPath = Path.Combine(currentRoot, "LehmanLaidun.Console");
-            return slnPath;
         }
     }
 }
---
+++ Source/Tests/LehmanLaidun.Console.Test/TheFileSystem.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace LehmanLaidun.Console.Test
+{
+    /// <summary>This class contains functionality for handling the "physical" file system when testing.
+    /// </summary>
+    internal class TheFileSystem
+    {
+        /// <summary>This method executes the console application
+        /// with the parameters provided
+        /// and returns the result.
+        /// </summary>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
+        internal static string Execute(string arguments)
+        {
+            using (var p = new Process())
+            {
+                p.StartInfo.FileName = TheFileSystem.FindConsoleExecutable();
+                p.StartInfo.WorkingDirectory = TheFileSystem.FindTesteePrjPath();
+                p.StartInfo.Arguments = arguments;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.Start();
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                if (p.ExitCode != 0)
+                {
+                    output = output + Environment.NewLine + $"ExitCode=[{p.ExitCode}]{Environment.NewLine}{output}";
+                }
+                return output.NormaliseLineEndings();
+            }
+        }
+
+        /// <summary>This method returns the full path to the Data folder, the folder where test data is kept.
+        /// </summary>
+        /// <returns></returns>
+        internal static string FindDataPath()
+        {
+            var exeDirectory = Path.Combine(FindTesteePrjPath(), "Data");
+            return exeDirectory;
+        }
+
+        /// <summary>This method returns the binary path, the one edning with "netcoreapp...".
+        /// This method will return the wrong result when the dotnet version is updated.
+        /// </summary>
+        /// <returns></returns>
+        private static string FindConsoleBinPath()
+        {
+#if DEBUG
+            var releaseDebugFolder = "Debug";
+#endif
+#if RELEASE
+            var releaseDebugFolder = "Release";
+#endif
+            var exeDirectory = Path.Combine(FindTesteePrjPath(), "bin", releaseDebugFolder, "netcoreapp3.1");
+            return exeDirectory;
+        }
+
+        /// <summary>This method returns the name of the executable.
+        /// It can handle several operating systems and throws an exception
+        /// if un unknown operating system is encountered.
+        /// </summary>
+        /// <returns></returns>
+        private static string FindConsoleExecutable()
+        {
+            var fileEnding = Environment.OSVersion.Platform switch
+            {
+                PlatformID.Win32NT => ".exe",
+                PlatformID.Win32Windows => ".exe",  //  Not tested.
+                PlatformID.Unix => ".Console",
+                PlatformID.MacOSX => ".Console",    // Not tested.
+                PlatformID.Win32S => throw new NotImplementedException(),
+                PlatformID.WinCE => throw new NotImplementedException(),
+                PlatformID.Xbox => throw new NotImplementedException(),
+                _ => throw new NotImplementedException(),
+            };
+            var exeFileName = Directory.GetFiles(FindConsoleBinPath())
+                .First(x => x.EndsWith(fileEnding));   // Drill down and hope for the dotnet version and file name to be as anticipated.
+            return exeFileName;
+        }
+
+        /// <summary>This method returns the path of the csproj file. Not including the csproj file.
+        /// </summary>
+        /// <returns></returns>
+        private static string FindTesteePrjPath()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory(); // Get the test directory.
+            var currentRoot = Path.Combine(currentDirectory, "..", "..", "..", "..", "..");    // Go to the closes common parent.
+            var prjPath = Path.Combine(currentRoot, "LehmanLaidun.Console");
+            return prjPath;
+        }
+    }
+}
