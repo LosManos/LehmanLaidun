884f8805563b6e39490e0deb97ee77f55d8da52a
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2020-12-19 22:49:07 +0000

    Add ImagePlugin
    This commit adds an Image plugin.
    It still does not output anything but that is for a future release.

---
+++ Source/Plugins/ImagePlugin/Command.cs
@@ -0,0 +1,22 @@
+﻿using LehmanLaidun.Plugin;
+using System.Xml.Linq;
+
+namespace ImagePlugin
+{
+    public class Command : ICommand
+    {
+        public string Name => "Image plugin";
+
+        public string Description => "This plugin returns image information.";
+
+        public ParseResult Parse(string pathfile)
+        {
+            var xml = new XDocument(
+                new XElement("image")
+            );
+            return ParseResult.Create(
+                Name,
+                xml);
+        }
+    }
+}
