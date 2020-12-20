14208e4afefbdceb93c738dbbd04f1ed644c80d1
LosManos <ola.fjelddahl@(diespammers)gmail.com>
2020-12-20 19:43:57 +0000

    ImagePlugin returns width and height
    This commit makes the ImagePlugin return the images's width and height.

--- Source/Plugins/ImagePlugin/Command.cs
+++ Source/Plugins/ImagePlugin/Command.cs
@@ -1,4 +1,5 @@
 ﻿using LehmanLaidun.Plugin;
+using System.Drawing;
 using System.Xml.Linq;
 
 namespace ImagePlugin
@@ -11,9 +12,21 @@ namespace ImagePlugin
 
         public ParseResult Parse(string pathfile)
         {
+            var image = Image.FromFile(pathfile);
+
             var xml = new XDocument(
-                new XElement("image")
+                new XElement("image",
+                    new XAttribute("width", image.Width),
+                    new XAttribute("height", image.Height)
+                )
             );
+
+            //  There is more info for an image.
+            //  One is Flags https://docs.microsoft.com/en-us/dotnet/api/system.drawing.image.flags
+            //  Another is properties.
+            //  https://docs.microsoft.com/en-us/dotnet/desktop/winforms/advanced/how-to-read-image-metadata
+            //  https://docs.microsoft.com/en-us/dotnet/api/system.drawing.imaging.propertyitem.id
+
             return ParseResult.Create(
                 Name,
                 xml);
